9457ec20a7cc95b2579ff2261fcc9f8807b012ff
Beckys <rtettey19@gmail.com>
2025-10-01 12:48:14 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// string
+let name = "rebecca"
+let hobbies = "reading"
+let friends = "alice"
+let food = "pizza"
+
+
+
+// number
+let age = 19
+let class = 40
+let cities = 20
+let pets = 2
+
+
+
+
+// boolean
+let isStudent = true
+let  fun = false
+let life = true
+let happy = true
